b61b46d4c81a4c6b118d07af4d424477290bfd73
Abhishek Khangare <abhishekkhangare21@gmail.com>
2023-12-21 09:04:57 +0530

    add navlink

---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "./Header.module.scss";
+import { Link, NavLink } from "react-router-dom";
+
+// this is called js wrapper
+const logo = (
+  <div className={styles.logo}>
+    <Link to="/">
+      <h2>
+        Shop<span>Ito</span>
+      </h2>
+    </Link>
+  </div>
+);
+const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
+
+const Header = () => {
+  return (
+    <header>
+      <div className={styles.header}>
+        {logo}
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/shop" className={activeLink}>
+                shop
+              </NavLink>
+            </li>
+          </ul>
+          <div className={styles["header-right"]}>
+            <span className={styles.links}>
+              <NavLink to={"login"} className={activeLink}>
+                Login
+              </NavLink>
+              <NavLink to={"register"} className={activeLink}>
+                Register
+              </NavLink>
+              <NavLink to={"order-history"} className={activeLink}>
+                My Order
+              </NavLink>
+            </span>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
