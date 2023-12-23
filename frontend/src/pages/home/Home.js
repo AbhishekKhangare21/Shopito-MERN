7ab3814b8c846a17e5d29f3e0f771d86658e29f8
Abhishek Khangare <abhishekkhangare21@gmail.com>
2023-12-23 06:15:54 +0530

    feat: slider

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Slider from "../../components/slider/Slider";
 
 const Home = () => {
-  return <h1>Home Page!!</h1>;
+  return (
+    <>
+      <Slider />
+    </>
+  );
 };
 
 export default Home;
