4e380430062a4b3926b37788d3d7d12a0eb3aa5e
Abhishek Khangare <abhishekkhangare21@gmail.com>
2023-12-24 09:36:33 +0530

    feat : Usermodel

---
+++ backend/models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please add a email"],
+    unique: true,
+    trim: true,
+    match: [
+      /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+      "Please enter a valid email",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minLength: [6, "Password must be up to 6 characters"],
+  },
+  role: {
+    type: String,
+    required: [true],
+    default: "customer",
+    enum: ["customer", "admin"],
+  },
+  photo: {
+    type: String,
+    required: [true, "Please add a photo"],
+    default:
+      "https://thumbs.dreamstime.com/b/default-avatar-profile-image-vector-social-media-user-icon-potrait-182347582.jpg",
+  },
+  phone: {
+    type: String,
+    default: "+91",
+  },
+  address: {
+    type: Object,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
